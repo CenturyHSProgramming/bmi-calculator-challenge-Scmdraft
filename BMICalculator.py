93f8fdc6823a77be24da9738f2ba6161f2db1bb0
Laurins_Matthew <laurm234@hsd.k12.or.us>
2017-05-11 10:43:37 -0700

    adding successful code

--- BMICalculator.py
+++ BMICalculator.py
@@ -5,6 +5,21 @@
 # Define Function below
 # be sure to return an integer
 
+def calculateBMI(height, weight):
+    answer = (weight / (height)**2) * 703
+    answer = round(answer, 0)
+    return(answer)
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     # Call the function in here if you want to test it
